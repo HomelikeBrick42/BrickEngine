65b1b6a49311bda35192665bf9ddeac4483c3fe8
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2020-07-22 18:07:39 +1200

    Entry Point Added

--- BrickEngine/src/BrickEngine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use by BrickEngine Applications
-
-#include "BrickEngine/Application.h"
-
-// ---Entry Point----------------------------------------
-#include "BrickEngine/EntryPoint.h"
-// ------------------------------------------------------
